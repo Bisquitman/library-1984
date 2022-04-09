a2e93fbb618a165af7a79278be93da1a6a6a9d80
Bisquitman <lynx8309@gmail.com>
2022-04-09 14:33:14 +0300

    Day 3

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,46 @@
-import {sum} from './modules/test.js';
+import Navigo from 'navigo';
 
+const library = document.querySelector('.library');
+const book = document.querySelector('.book');
+const add = document.querySelector('.add');
+const addBtns = document.querySelectorAll('.header__btn-add, .library__add-btn');
+const backBtns = document.querySelectorAll('.header__btn-back');
+
+const router = new Navigo('/', {
+  hash: true,
+});
+
+const closeAllPages = () => {
+  library.classList.add('hide');
+  book.classList.add('hide');
+  add.classList.add('hide');
+};
+
+router.on({
+  '/': () => {
+    closeAllPages();
+    library.classList.remove('hide');
+  },
+  'book': () => {
+    closeAllPages();
+    book.classList.remove('hide');
+  },
+  'add': () => {
+    closeAllPages();
+    add.classList.remove('hide');
+  },
+}).resolve();
+
+addBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    router.navigate('add');
+  });
+});
+
+backBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    router.navigate('/');
+  });
+});
