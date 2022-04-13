b8fc236941e3e522f86fff69963d96e4af91b3d4
Bisquitman <78824565+Bisquitman@users.noreply.github.com>
2022-04-13 14:09:31 +0300

    Day 5

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,13 @@ const backBtns = document.querySelectorAll('.header__btn-back');
 const searchBtns = document.querySelectorAll('.header__btn-search');
 const search = document.querySelector('.search');
 
+const fieldsBtnSort = document.querySelector('.fields__btn_sort');
+const fieldsListSort = document.querySelector('.fields__list_sort');
+const fieldsBtnFilter = document.querySelector('.fields__btn_filter');
+const fieldsListFilter = document.querySelector('.fields__list_filter');
+
+const form = document.querySelector('.add__form');
+
 const router = new Navigo('/', {
   hash: true,
 });
@@ -16,21 +23,23 @@ const closeAllPages = () => {
   library.classList.add('hide');
   book.classList.add('hide');
   add.classList.add('hide');
-  search.classList.remove('search_active');
 };
 
 router.on({
   '/': () => {
     closeAllPages();
     library.classList.remove('hide');
+    document.body.classList.remove('body_gradient')
   },
   'book': () => {
     closeAllPages();
     book.classList.remove('hide');
+    document.body.classList.add('body_gradient')
   },
   'add': () => {
     closeAllPages();
     add.classList.remove('hide');
+    document.body.classList.add('body_gradient')
   },
 }).resolve();
 
@@ -45,6 +54,7 @@ backBtns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     e.preventDefault();
     router.navigate('/');
+    form.reset();
   });
 });
 
@@ -60,6 +70,63 @@ const closeSearch = ({target}) => {
 searchBtns.forEach((btn) => {
   btn.addEventListener('click', () => {
     search.classList.add('search_active');
-    document.body.addEventListener('click', closeSearch);
+    document.body.addEventListener('click', closeSearch, true);
   });
 });
+
+const controlField = (btn, list, offList) => {
+  btn.addEventListener('click', () => {
+    list.classList.toggle('fields__list_active');
+    offList.classList.remove('fields__list_active');
+  });
+
+  list.addEventListener('click', ({target}) => {
+    if (target.classList.contains('fields__btn')) {
+      list.classList.remove('fields__list_active');
+    }
+  });
+};
+
+controlField(fieldsBtnSort, fieldsListSort, fieldsListFilter);
+controlField(fieldsBtnFilter, fieldsListFilter, fieldsListSort);
+
+const changeFieldset = () => {
+  const fieldsets = document.querySelectorAll('.add__fieldset');
+  const addBtn = document.querySelector('.add__btn');
+  let count = 0;
+
+  addBtn.addEventListener('click', ({target}) => {
+    const fieldset = fieldsets[count];
+    let valid = true;
+
+    for (const element of fieldset.elements) {
+      if (!element.checkValidity()) {
+        element.classList.add('not-valid');
+        valid = false;
+      } else {
+        element.classList.remove('not-valid');
+      }
+    }
+
+    if (valid) {
+      count += 1;
+      if (count >= fieldsets.length - 1) {
+        addBtn.textContent = 'Добавить книгу';
+      }
+      if (count >= fieldsets.length) {
+        const data = true;  // данные с сервера
+        if (data) {
+          form.reset();
+          router.navigate('/');
+          count = 0;
+          addBtn.textContent = 'Далее';
+        }
+      }
+
+      fieldset.classList.add('hide');
+      fieldsets[count].classList.remove('hide');
+    }
+  });
+};
+
+changeFieldset();
